a0d0443f4da80762bfd50f3d628ed6ca810687c5
DilshanHerath <dilshanmadushanka981@gmail.com>
2025-09-13 09:42:05 +0530

    Add Invite model and migration for invites table

---
+++ app/Models/Invite.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Invite extends Model
+{
+    protected $fillable = [
+        'name',
+        'company',
+        'number_of_invites',
+        'contact',
+        'email',
+        'table_no',
+    ];
+}
